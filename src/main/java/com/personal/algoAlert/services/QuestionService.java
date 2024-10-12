8de6c8bfcdf4a375bf0231722f45650ce78c5d0c
Rashmi Sharma <rashmisharmaofficial19019@gmail.com>
2024-10-12 17:29:20 +0530

    Added calculate today's questions

--- src/main/java/com/personal/algoAlert/services/QuestionService.java
+++ src/main/java/com/personal/algoAlert/services/QuestionService.java
@@ -5,12 +5,17 @@ import com.personal.algoAlert.shared.helpers.DateTimeConverter;
 import com.personal.algoAlert.shared.model.QuestionRecords;
 import com.personal.algoAlert.system.repositories.QuestionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Criteria;
 
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -38,22 +43,28 @@ public class QuestionService {
         //query to get filtered data
         Query query = new Query();
         query.addCriteria(Criteria.where("firebase_uid").is(firebaseUid));
-       if(searchQuery != null && !searchQuery.isEmpty()){
+
+        //add search query if search string is present
+        if(searchQuery != null && !searchQuery.isEmpty()){
            query.addCriteria(Criteria.where("quesName").regex(searchQuery, "i"));
-       }
+        }
 
+        //add sorting by 'quesLastAttemptDate', descending (latest first)
+        query.with(Sort.by(Sort.Direction.DESC, "quesLastAttemptDate"));
+
+        //pagination
         query.skip(page * size).limit(size);
-       List<QuestionRecords> questions = mongoTemplate.find(query, QuestionRecords.class);
 
-       //prepare response
+        //execute the query
+        List<QuestionRecords> questions = mongoTemplate.find(query, QuestionRecords.class);
+
+        //prepare response
         Map<String, Object> response = new HashMap<>();
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("response_data", questions);
         responseData.put("totalCount", totalCount);
 
-//        response.put("response", responseData);
         return responseData;
-        //        return questionsRepository.findAll();
     }
 
     // Business logic for retrieving a question by ID
@@ -139,10 +150,51 @@ public class QuestionService {
         }).orElse(null));
     }
 
-
     private String calculateLastAttemptDate(QuestionRecordReqDto dto) {
         // Your logic to calculate last attempt date
         return "2024-01-02"; // Example
+    }
+
+    public Map<String, Object> questionsToAttemptToday(String firebaseUid, String searchQuery, int page, int size){
+        LocalDate today = LocalDate.now();
+        ZonedDateTime startOfDay = today.atStartOfDay(ZoneId.systemDefault());
+        ZonedDateTime endOfDay = today.plusDays(1).atStartOfDay(ZoneId.systemDefault());
+
+        Date startDate = Date.from(startOfDay.toInstant());
+        Date endDate = Date.from(endOfDay.toInstant());
+
+        Query countQuery = new Query();
+        countQuery.addCriteria(Criteria.where("firebase_uid").is(firebaseUid));
+        long totalCount = mongoTemplate.count(countQuery, QuestionRecords.class);
+
+        //query to get filtered data
+        //2024-09-11 16:58:10
+        Query query = new Query();
+        query
+                .addCriteria(Criteria.where("firebase_uid").is(firebaseUid))
+                .addCriteria(Criteria.where("quesNextAttemptDate").gte(startDate).lte(endDate));
+
+        //add search query if search string is present
+        if(searchQuery != null && !searchQuery.isEmpty()){
+            query.addCriteria(Criteria.where("quesName").regex(searchQuery, "i"));
+        }
+
+        //add sorting by 'quesLastAttemptDate', descending (latest first)
+        query.with(Sort.by(Sort.Direction.DESC, "quesLastAttemptDate"));
+
+        //pagination
+        query.skip(page * size).limit(size);
+
+        //execute the query
+        List<QuestionRecords> questions = mongoTemplate.find(query, QuestionRecords.class);
+
+        //prepare response
+        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("response_data", questions);
+        responseData.put("totalCount", totalCount);
+
+        return responseData;
     }
 
 
